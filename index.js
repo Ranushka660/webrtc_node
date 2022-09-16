d58f9b970d5ddefdf4fa12cd33e8407a8d052200
Ranushka660 <41850704+Ranushka660@users.noreply.github.com>
2022-09-16 10:01:11 +0530

    add new

---
+++ index.js
@@ -0,0 +1,40 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin:'*',
+        methods:['GET', 'POST']
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+})
+
+app.get('/user', (req, res) => {
+    console.log("user name");
+})
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded');
+    })
+
+    socket.on('calluser', (userToCall, signalData, from, name) => {
+        io.to(userToCall).emit('calluser', {signal:signalData, from, name});
+    })
+
+    socket.on('answercall', (data)=> {
+        io.to(data.to).emit('callaccepted', data.signal);
+    })
+})
+
+server.listen(PORT, () => console.log(`Listning Port ${PORT}`));
